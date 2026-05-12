b6a3f079228a6e6f5bee2a36c71ec4a2dfdab604
nicopez <NicoPez@users.noreply.github.com>
2026-05-12 20:11:00 -0300

    feat: initialize Vite frontend and Node backend architectures for Live Markets MVP

---
+++ goalchain_app/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for solana web3js to work in browser
+    'process.env': {},
+    global: 'window',
+  },
+})
